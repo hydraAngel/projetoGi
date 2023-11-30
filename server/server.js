3c5e9cc7b06a4e6d768277a96853b72d21418224
Pedro Paiva Verissimo <paiva@pop-os.localdomain>
2023-11-30 19:59:17 -0300

    real

---
+++ server/server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const { log } = console;
+var htp = require("html-pdf-node");
+// var pdf = require("pdf-creator-node");
+// var fs = require("fs");
+
+// var html = fs.readFileSync("template.html", "utf8");
+
+var con = mysql.createConnection({
+    host: "193.123.127.248",
+    user: "root",
+    password: "831842",
+    database: "stg",
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/alunos", (req, res) => {
+    con.query("SELECT * FROM alunos", function (err, result, fields) {
+        if (err) throw err;
+        log(result);
+        res.json(result);
+    });
+});
+
+app.get("/alunos/:codAlu", (req, res) => {
+    var isNum = /^\d+$/.test(req.params.codAlu);
+    // log(/^\d+$/.test(req.params.codAlu))
+    if (isNum) {
+        con.query(
+            "SELECT * FROM alunos WHERE cod_alu=" + parseInt(req.params.codAlu),
+            function (err, result, fields) {
+                if (err) throw err;
+                log(result);
+                res.json(result);
+            }
+        );
+    } else {
+        con.query(
+            "SELECT * FROM alunos WHERE nome LIKE '" + req.params.codAlu + "%'",
+            function (err, result, fields) {
+                if (err) throw err;
+                log(result);
+                res.json(result);
+            }
+        );
+    }
+});
+
+app.get("/alunos/notas/:codAlu", (req, res) => {
+    con.query(
+        "SELECT nome, nota, falta, cursou FROM alunota JOIN materias ON alunota.cod_mat = materias.cod_mat WHERE cod_alu=" +
+            parseInt(req.params.codAlu) +
+            " ORDER BY alunota.cod_mat;",
+        function (err, result, fileds) {
+            if (err) throw err;
+            log(result);
+            res.json(result);
+        }
+    );
+});
+
+app.listen(8000, () => {
+    console.log(`Server na porta 8000 `);
+});
