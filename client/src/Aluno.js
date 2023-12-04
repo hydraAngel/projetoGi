f7e40e8f07497be983efa584ac76642ed5a7b575
hydraAngel <ppvverissimo@gmail.com>
2023-12-04 00:56:18 -0300

    basico

---
+++ client/src/Aluno.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./Aluno.css";
+
+function Aluno(){
+  return (
+    <>
+      <h1>Estamos adicionando mais coisas.</h1>
+      <p>Ass: Programador</p>
+    </>
+  )
+}
