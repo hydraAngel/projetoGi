7099dc910c9a8a747365cb267d41e9db4d3cf5da
hydraAngel <ppvverissimo@gmail.com>
2023-12-04 23:50:31 -0300

    algumas mudanças no front

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/boletim">Boletim</Link>
-        </li>
-        <li>
-          <Link to="/alunos">Alunos</Link>
-        </li>
-        {/* Adicione mais links conforme necessário */}
-      </ul>
-    </nav>
+    <div class="nav">
+      <input type="checkbox" id="nav-check"></input>
+      <div class="nav-header">
+        <div class="nav-title">
+          STG
+        </div>
+      </div>
+      <div class="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      
+      <div class="nav-links">
+        <Link to="/" >Home</Link>
+        <Link to="/boletim" >Boletim</Link>
+        <Link to="/alunos" >Alunos</Link>
+        {/* <Link to="https://in.linkedin.com/in/jonesvinothjoseph" >LinkedIn</Link> */}
+        {/* <Link to="https://codepen.io/jo_Geek/" >Codepen</Link> */}
+        {/* <Link to="https://jsfiddle.net/user/jo_Geek/" >JsFiddle</Link> */}
+      </div>
+    </div>
   );
 }
 
