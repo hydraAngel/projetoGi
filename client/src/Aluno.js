cd2cef12c02eeee628015c8aa8de7901cfb109a7
hydraAngel <64570217+hydraAngel@users.noreply.github.com>
2023-12-13 22:08:46 -0300

    s

--- client/src/Aluno.js
+++ client/src/Aluno.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import "./Aluno.css";
 import { Cadastrar } from './Cadastrar.js'
-import { listaAlunos } from './ListaAlunos.js'
 
-function Aluno(){
-  console.log(listaAlunos())
+function Aluno() {
+  fetch("https://verissimos.ddnsfree.com:8080/lista")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    });
   return (
     <>
-      
+
     </>
   )
 }
--- client/src/ListaAlunos.js
+++
@@ -1,7 +0,0 @@
-export function listaAlunos() {
-    fetch("https://verissimos.ddnsfree.com:8080/lista")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-        });
-}
