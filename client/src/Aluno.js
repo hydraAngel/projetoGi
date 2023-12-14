6ecb9703d0b37dc808c23002031bebf7f8cc3999
hydraAngel <64570217+hydraAngel@users.noreply.github.com>
2023-12-13 22:17:48 -0300

    sync

--- client/src/Aluno.js
+++ client/src/Aluno.js
@@ -6,7 +6,7 @@ function Aluno() {
   fetch("https://verissimos.ddnsfree.com:8080/lista")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      
     });
   return (
     <>
