d90b035708889114d83e87f7390c67969c070336
hydraAngel <64570217+hydraAngel@users.noreply.github.com>
2023-12-13 22:07:01 -0300

    a

--- client/src/ListaAlunos.js
+++ client/src/ListaAlunos.js
@@ -2,6 +2,6 @@ export function listaAlunos() {
     fetch("https://verissimos.ddnsfree.com:8080/lista")
         .then((response) => response.json())
         .then((data) => {
-            return data
+            console.log(data)
         });
 }
